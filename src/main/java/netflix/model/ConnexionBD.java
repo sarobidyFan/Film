d345f89c8ce1ad6bebbfff2445fce7735f9ceb40
Sarobidy <hei.sarbidy.8@gmail.com>
2026-07-02 16:24:26 +0300

    Suppression mdp

--- src/main/java/netflix/model/ConnexionBD.java
+++ src/main/java/netflix/model/ConnexionBD.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 public class ConnexionBD {
     private static final String URL = "jdbc:postgresql://localhost:5432/Netflix";
     private static final String USER = "postgres";
-    private static final String PASSWORD = "rakoto";
+    private static final String PASSWORD = "";
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
